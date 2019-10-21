701d45ec28f716c3756be8894485bdfc0b006d27
F.oris <oris@itsanr.com>
2019-10-21 16:19:38 +0800

    优化Response响应状态码硬编码逻辑，响应状态码定义提取到配置文件

--- src/Exceptions/ErrorException.php
+++ src/Exceptions/ErrorException.php
@@ -2,45 +2,10 @@
 
 namespace Foris\LaExtension\Exceptions;
 
-use Throwable;
-use Symfony\Component\HttpFoundation\Response as FoundationResponse;
-
 /**
  * Class ErrorException
  */
 class ErrorException extends BaseException
 {
-    /**
-     * ErrorException constructor.
-     * @param string         $message
-     * @param int            $code
-     * @param Throwable|null $previous
-     * @param array          $data
-     */
-    public function __construct(
-        string $message = "",
-        int $code = FoundationResponse::HTTP_INTERNAL_SERVER_ERROR,
-        Throwable $previous = null,
-        array $data = []
-    ) {
-        parent::__construct($message, $code, $previous, $data);
-    }
 
-    /**
-     * 获取异常数据
-     *
-     * @return array
-     */
-    public function getData()
-    {
-        $data = parent::getData();
-        $previous = $this->getPrevious();
-
-        if ($previous
-            && env('APP_DEBUG', false)) {
-            $data['previous'] = get_class($previous);
-        }
-
-        return $data;
-    }
 }
--- src/Exceptions/NotFoundHttpException.php
+++ src/Exceptions/NotFoundHttpException.php
@@ -2,7 +2,6 @@
 
 namespace Foris\LaExtension\Exceptions;
 
-use Throwable;
 use Symfony\Component\HttpFoundation\Response as FoundationResponse;
 
 /**
@@ -11,31 +10,22 @@ use Symfony\Component\HttpFoundation\Response as FoundationResponse;
 class NotFoundHttpException extends BaseException
 {
     /**
-     * NotFoundHttpException constructor.
+     * 获取默认调用的Response方法
      *
-     * @param string         $message
-     * @param int            $code
-     * @param Throwable|null $previous
-     * @param array          $data
+     * @return string
      */
-    public function __construct(
-        string $message = "",
-        int $code = FoundationResponse::HTTP_NOT_FOUND,
-        Throwable $previous = null,
-        array $data = []
-    ) {
-        parent::__construct($message, $code, $previous, $data);
+    protected function getDefaultResponseMethod()
+    {
+        return 'notFound';
     }
 
     /**
-     * 找不到路由异常
+     * 获取默认响应的状态码
      *
-     * @return \Illuminate\Http\JsonResponse
+     * @return \Illuminate\Config\Repository|mixed
      */
-    public function render()
+    protected function getDefaultResponseCode()
     {
-        $response = parent::render();
-        $response->setStatusCode(FoundationResponse::HTTP_NOT_FOUND);
-        return $response;
+        return config('app-ext.api_response_code.notFound', FoundationResponse::HTTP_NOT_FOUND);
     }
 }
